907e03a339fad5104f2569a00e4e5ae495349764
Joshua-B <joshua_bloom96@yahoo.co.uk>
2022-05-17 13:21:35 +0100

    basic logic added

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+//variables
+let total = 0;
+let locked = 0;
+let cardArr = [];
+
+//dom elements
+const drawnCardDisplay = document.getElementById("drawnCard");
+const totalScoreDisplay = document.getElementById("totalScore");
+
+//very simple function, to get random 'card' can be made more complex
 const getRandomCard = () => {
-    return (Math.floor(Math.random() * 11) + 1) //very simple way of getting random card
+    return (Math.floor(Math.random() * 11) + 1); 
 }
 
-console.log(getRandomCard());
+//pull a card, adds it to array and checks for >21
+const combineCards = () => {
+    let pull = getRandomCard();
+    cardArr.push(pull);
+    total += pull;
+    if(total > 21){
+        total = "Try Again";
+        return "You Bust!";
+    }
+    return  "pulled cards: " + cardArr;
+}
+
+//event listeners
+document.getElementById("drawBtn").addEventListener("click", () => {
+    drawnCardDisplay.innerHTML =  combineCards(),
+        totalScoreDisplay.innerHTML = total;
+});
+
+document.getElementById("standBtn").addEventListener("click", () => {locked = total;});
